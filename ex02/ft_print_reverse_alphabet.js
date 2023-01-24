d065797240862ecd33d86d1b3a3b7a760029c5cf
Alban Huntziger <albanhuntziger@MacBook-Air-de-Alban.local>
2023-01-24 16:25:40 +0100

    ex02

--- ex02/ft_print_reverse_alphabet.js
+++ ex02/ft_print_reverse_alphabet.js
@@ -1,8 +1,7 @@
 const generateAlphabet = () => {
     let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
-    let result = alphabet.join('')
+    let result = alphabet.reverse().join('')
     return result
 }
 
 console.log(generateAlphabet())
-
