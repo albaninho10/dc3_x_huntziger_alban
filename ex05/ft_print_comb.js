83b88b8a452f592ec52bb49b7004d7fb14e397a0
Alban Huntziger <albanhuntziger@MacBook-Air-de-Alban.local>
2023-01-24 17:45:58 +0100

    ex05

---
+++ ex05/ft_print_comb.js
@@ -0,0 +1,16 @@
+const printCombinaisons = () => {
+    let array = []
+    for (let i = 0; i <= 9; i++) {
+        for (let j = i + 1; j <= 9; j++) {
+            for (let k = j + 1; k <= 9; k++) {
+                let str = i
+                array.push("" + i + j + k);
+            }
+        }
+    }
+
+    let result = array.join()
+    return result
+}
+
+console.log(printCombinaisons())
